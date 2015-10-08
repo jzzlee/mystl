4ec299c4d7bebd1f0db8ac2dedd1354caa862046
jzzlee <jzzlee@163.com>
2015-10-08 22:24:26 +0800

    v1.8.2

--- ConsoleApplication2/my_map.h
+++ ConsoleApplication2/my_map.h
@@ -10,8 +10,6 @@ namespace my_stl
 	using std::size_t;
 	using std::ptrdiff_t;
 
-
-
 	//map声明
 	template<typename Key, typename T, typename Compare, typename Allocator>
 	class map;
@@ -62,9 +60,6 @@ namespace my_stl
 		class value_compare
 		{
 		public:
-			typedef bool result_type;
-			typedef value_type first_argument_type;
-			typedef value_type second_argument_type;
 			friend class map < Key, T, Compare, Allocator > ;
 			bool operator()(const value_type& lhs, const value_type& rhs) const
 			{
@@ -100,7 +95,7 @@ namespace my_stl
 		map& operator=(std::initializer_list<value_type> ilist) { t = ilist; return *this; }
 
 		//Returns the allocator associated with the container. 
-		allocator_type get_allocator() const { return t.alloc; }
+		allocator_type get_allocator() const { return t.get_allocator(); }
 
 		iterator begin() { return t.begin(); }
 		const_iterator begin() const { return t.begin(); }
@@ -163,7 +158,7 @@ namespace my_stl
 		void insert(std::initializer_list<value_type> ilist) { t.insert_unique(ilist.begin(), ilist.end()); }
 
 		template< class... Args >
-		std::pair<iterator, bool> emplace(Args&&... args)
+		pair<iterator, bool> emplace(Args&&... args)
 		{
 			return insert(std::forward<Args>(args)...);
 		}
@@ -194,6 +189,8 @@ namespace my_stl
 
 		key_compare key_comp() const { return t.key_comp(); }
 		value_compare value_comp() const { return value_compare(key_comp()); }
+
+		void print() { t.inprint(); }
 
 	private:
 		tree_type t;
@@ -241,6 +238,179 @@ namespace my_stl
 		std::swap(lhs, rhs);
 	}
 
+
+
+	//multimap声明
+	template<typename Key, typename T, typename Compare, typename Allocator>
+	class multimap;
+
+	//比较运算声明
+	template< typename Key, typename T, typename Compare, typename Alloc >
+	bool operator==(const multimap<Key, T, Compare, Alloc>& lhs, const multimap<Key, T, Compare, Alloc>& rhs);
+	template< typename Key, typename T, typename Compare, typename Alloc >
+	bool operator<(const multimap<Key, T, Compare, Alloc>& lhs, const multimap<Key, T, Compare, Alloc>& rhs);
+
+	//multimap定义
+	template<typename Key, typename T, typename Compare = std::less<Key>, typename Allocator = my_stl::allocator<pair<const Key, T> > >
+	class multimap
+	{
+	public:
+		typedef multimap<Key, T, Compare, Allocator> Myt;
+		typedef Key key_type;
+		typedef T mapped_type;
+		typedef pair<const Key, T> value_type;
+		typedef size_t size_type;
+		typedef ptrdiff_t difference_type;
+		typedef Compare key_compare;
+		typedef Allocator allocator_type;
+
+		//友元声明
+		template< typename Key, typename T, typename Compare, typename Alloc >
+		friend bool operator==(const multimap<Key, T, Compare, Alloc>& lhs, const multimap<Key, T, Compare, Alloc>& rhs);
+		template< typename Key, typename T, typename Compare, typename Alloc >
+		friend bool operator<(const multimap<Key, T, Compare, Alloc>& lhs, const multimap<Key, T, Compare, Alloc>& rhs);
+
+		//从value选出Key
+		class select1st
+		{
+		public:
+			Key operator()(const value_type &p){ return p.first; }
+		};
+
+		typedef value_type& reference;
+		typedef const value_type& const_reference;
+		typedef typename Allocator::pointer pointer;
+		typedef typename Allocator::const_pointer const_pointer;
+		typedef rb_tree<key_type, value_type, select1st, key_compare, Allocator> tree_type;
+		typedef typename tree_type::iterator iterator;
+		typedef typename tree_type::const_iterator const_iterator;
+		typedef bidirectional_iterator_tag iteratore_category;
+
+		//value
+		class value_compare
+		{
+		public:
+			friend class multimap < Key, T, Compare, Allocator > ;
+			bool operator()(const value_type& lhs, const value_type& rhs) const
+			{
+				return comp(lhs.first, rhs.first);
+			}
+
+		protected:
+			Compare comp;
+			value_compare(Compare c) : comp(c) {}
+
+		};
+
+		explicit multimap(const Compare &comp = Compare(), const Allocator &alloc = Allocator()) : t(comp, alloc) {}
+		explicit multimap(const Allocator& alloc) : t(alloc) {}
+		
+		template< class InputIterator >
+		multimap(InputIterator first, InputIterator last,
+			const Compare& comp = Compare(),
+			const Allocator& alloc = Allocator())
+			: t(comp, alloc) { t.insert_equal(first, last); }
+		multimap(const multimap& other) : t(other.t) {}
+		multimap(const multimap &ither, const Allocator &alloc) : t(other.t, alloc) {}
+		multimap(multimap&& other) : t(std::move(other.t)) {}
+		multimap(multimap&& other, const Allocator& alloc) : t(std::move(other.t), alloc) {}
+		multimap(std::initializer_list<value_type> init,
+			const Compare& comp = Compare(),
+			const Allocator& alloc = Allocator())
+			: t(comp, alloc) { t.insert_equal(init.begin(), init.end()); }
+
+		multimap& operator=(const multimap& other){ t = other.t; return *this; }
+		multimap& operator=(multimap&& other) { t = std::move(other.t); return *this; }
+		multimap& operator=(std::initializer_list<value_type> ilist) { t = ilist; return *this; }
+		allocator_type get_allocator() const { return t.get_allocator(); }
+
+		iterator begin() { return t.begin(); }
+		const_iterator begin() const { return t.begin(); }
+		const_iterator cbegin() const { return ((const Myt*)this)->begin(); }
+		iterator end() { return t.end(); }
+		const_iterator end() const { return t.end(); }
+		const_iterator cend() const { return ((const Myt*)this)->end(); }
+		bool empty() const { return t.empty(); }
+		size_type size() const { return t.size(); }
+		size_type max_size() const { return t.max_size(); }
+
+		void clear() { t.clear(); }
+		iterator insert(const value_type &value) { return t.insert_equal(value);  }
+		iterator insert(value_type&& value) { return t.insert_equal(std::move(value)); }
+		iterator insert(const_iterator hint, const value_type &value) { return t.insert_equal(hint, value); }
+		iterator insert(const_iterator hint, value_type&& value) { return t.insert_equal(hint, std::move(value)); }
+		template< class InputIt >
+		void insert(InputIt first, InputIt last) { t.insert_equal(first, last); }
+		void insert(std::initializer_list<value_type> ilist) { t.insert_equal(ilist.begin(), ilist.end()); }
+		
+		template< class... Args >
+		iterator emplace(Args&&... args) { return insert(std::forward<Args>(args)...); }
+
+		template<typename ...Args>
+		iterator emplace_hint(const_iterator hint, Args&... args) { return insert(hint, std::forward<Args>(args)...); }
+
+		iterator erase(iterator pos) { return t.erase(pos); }
+		iterator erase(const_iterator pos) { return t.erase(pos); }
+		iterator erase(const_iterator first, const_iterator last) { return t.erase(first, last); }
+		size_type erase(const key_type& key) { return t.erase(key); }
+		void swap(multimap& other) { t.swap(other.t); }
+
+		size_type count(const Key& key) const { return t.count(key); }
+		iterator find(const Key& key) { return t.find(key); }
+		const_iterator find(const Key& key) const { return t.find(key); }
+
+		pair<iterator, iterator> equal_range(const Key& key) { return t.equal_range(key); }
+		pair<const_iterator, const_iterator> equal_range(const Key &key) const { return t.equal_range(key); }
+
+		iterator lower_bound(const Key &key) { return t.lower_bound(key); }
+		const_iterator lower_bound(const Key &key) const { return t.lower_bound(key); }
+
+		iterator upper_bound(const Key &key) { return t.upper_bound(key); }
+		const_iterator upper_bound(const Key &key) const { return t.upper_bound(key); }
+
+		key_compare key_comp() const { return t.key_comp(); }
+		value_compare value_comp() const { return value_compare(t.key_comp()); }
+
+
+	private:
+		tree_type t;
+	};
+
+	template< typename Key, typename T, typename Compare, typename Alloc >
+	bool operator==(const multimap<Key, T, Compare, Alloc>& lhs, const multimap<Key, T, Compare, Alloc>& rhs)
+	{
+		return lhs.t == rhs.t;
+	}
+
+	template< typename Key, typename T, typename Compare, typename Alloc >
+	bool operator!=(const multimap<Key, T, Compare, Alloc>& lhs, const multimap<Key, T, Compare, Alloc>& rhs)
+	{
+		return !(lhs == rhs);
+	}
+
+	template< typename Key, typename T, typename Compare, typename Alloc >
+	bool operator<(const multimap<Key, T, Compare, Alloc>& lhs, const multimap<Key, T, Compare, Alloc>& rhs)
+	{
+		return lhs.t < rhs.t;
+	}
+
+	template< typename Key, typename T, typename Compare, typename Alloc >
+	bool operator>(const multimap<Key, T, Compare, Alloc>& lhs, const multimap<Key, T, Compare, Alloc>& rhs)
+	{
+		return rhs < lhs;
+	}
+
+	template< typename Key, typename T, typename Compare, typename Alloc >
+	bool operator<=(const multimap<Key, T, Compare, Alloc>& lhs, const multimap<Key, T, Compare, Alloc>& rhs)
+	{
+		return !(rhs < lhs);
+	}
+
+	template< typename Key, typename T, typename Compare, typename Alloc >
+	bool operator>=(const multimap<Key, T, Compare, Alloc>& lhs, const multimap<Key, T, Compare, Alloc>& rhs)
+	{
+		return !(lhs < rhs);
+	}
 }
 
 #endif
