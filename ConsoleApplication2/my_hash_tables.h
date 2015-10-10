3ee7de7bdb8a8970bbd9026f2fd57879c6f57a3d
jzzlee <jzzlee@163.com>
2015-10-10 16:57:30 +0800

    v1.9.2

--- ConsoleApplication2/my_hash_tables.h
+++ ConsoleApplication2/my_hash_tables.h
@@ -12,6 +12,18 @@
 
 namespace my_stl
 {
+#define _MY_DEBUG
+
+	template<typename T>
+	class compare_equal
+	{
+	public:
+		bool operator()(const T &lhs, const T &rhs)
+		{
+			return lhs == rhs;
+		}
+	};
+
 	using std::size_t;
 	using std::ptrdiff_t;
 	using std::pair;
@@ -44,6 +56,9 @@ namespace my_stl
 		typedef value_type* pointer;
 		typedef typename vector<hashnode*>::iterator vector_iterator;
 
+		template<typename Key, typename Value, typename HashFcn, typename ExtractKey, typename EqualKey, typename Allocator>
+		friend class hash_table;
+
 		__hash_iterator() {};
 		__hash_iterator(hashnode *nd, hashtable * h) : node(nd), ht(h) {}
 		__hash_iterator(hashnode *nd, vector_iterator bl, hashtable * h) : node(nd), block(bl), ht(h) {}
@@ -71,7 +86,9 @@ namespace my_stl
 
 		bool operator==(const iterator &it) { return node == it.node; }
 		bool operator!=(const iterator &it) { return node != it.node; }
-
+#ifndef _MY_DEBUG
+	private:
+#endif
 		hashnode *node; //指向节点
 		vector_iterator block; //指向node所在的桶
 		hashtable *ht;
@@ -95,8 +112,8 @@ namespace my_stl
 
 		template<typename Key, typename Value, typename HashFcn, typename ExtractKey, typename EqualKey, typename Allocator>
 		friend struct __hash_iterator;
-		
-//	private:
+
+	private:
 		vector<hashnode*> buckets;
 		allocator<value_type> alloc;
 		allocator<hashnode> alloc_node;
@@ -118,7 +135,9 @@ namespace my_stl
 		hash_table(InputIt first, InputIt last, size_type bucket_count = 11, const HashFcn& hash = HashFcn(), const EqualKey& equal = EqualKey(),
 			const Allocator& alloc = Allocator())
 			: num_elements(), bucket_size(bucket_count), buckets(bucket_count), hash(hash), equals(equal), alloc(alloc)
-		{insert(first, last); }
+		{
+			insert(first, last);
+		}
 
 		hash_table(const hash_table &other)
 			: load_factor(other.load_factor), num_elements(other.num_elements), bucket_size(other.bucket_size), buckets(other.buckets),
@@ -136,10 +155,12 @@ namespace my_stl
 			: load_factor(other.load_factor), num_elements(other.num_elements), bucket_size(other.bucket_size), buckets(std::move(other.buckets)),
 			hash(other.hash), equals(other.equals), alloc(alloc) {}
 
-		hash_table(std::initializer_list<value_type> init, size_type bucket_count = 11, const HashFcn& hash = HashFcn(), 
+		hash_table(std::initializer_list<value_type> init, size_type bucket_count = 11, const HashFcn& hash = HashFcn(),
 			const EqualKey& equal = EqualKey(), const Allocator& alloc = Allocator())
-			: num_elements(), bucket_size(bucket_count), buckets(bucket_count), hash(hash), equals(equal), alloc(alloc) 
-		{ insert(init.begin(), init.end()); }
+			: num_elements(), bucket_size(bucket_count), buckets(bucket_count), hash(hash), equals(equal), alloc(alloc)
+		{
+			insert(init.begin(), init.end());
+		}
 
 		~hash_table()
 		{
@@ -169,7 +190,7 @@ namespace my_stl
 		iterator begin() { return iterator(buckets[0], buckets.begin(), this); }
 		const_iterator begin() const { return const_iterator(buckets[0], buckets.begin(), this); }
 		const_iterator cbegin() const { return ((const Myt*)this)->begin(); }
-		iterator end() { return iterator(buckets[buckets.size()], buckets.end(), this);  }
+		iterator end() { return iterator(buckets[buckets.size()], buckets.end(), this); }
 		const_iterator end() const { return const_iterator(buckets[buckets.size()], buckets.end(), this); }
 		const_iterator cend() const { return ((const Myt*)this)->end(); }
 		bool empty() const { return num_elements == 0; }
@@ -194,6 +215,9 @@ namespace my_stl
 			num_elements = 0;
 		}
 
+		//左值unique插入
+		//如果key已存在，返回pair(已存在key的iterator, false)
+		//否则，在key对应bucket的最后，返回pair(插入位置iterator, true).
 		pair<iterator, bool> insert_unique(const value_type &v)
 		{
 			size_type index = bkt_num(v);
@@ -201,27 +225,222 @@ namespace my_stl
 			if (!nd) //nd为空
 			{
 				buckets[index] = get_node(v);
-				return pair<iterator, bool>(iterator(buckets[index], vector_iterator(&buckets + index), this), true);
+				return pair<iterator, bool>(iterator(buckets[index], vector_iterator(&(buckets[0]) + index), this), true);
 			}
 			else //nd非空
 			{
-				if (equals(get_key(nd->value) == get_key(v))) //v的key已经存在
-					return pair<iterator, bool>(iterator(nd, vector_iterator(&buckets + index), this), false);
+				if (equals(get_key(nd->value), get_key(v))) //v的key已经存在
+					return pair<iterator, bool>(iterator(nd, vector_iterator(&(buckets[0]) + index), this), false);
 				while (nd->next) //nd->next非空
 				{
-					if (equals(get_key(nd->next->value) == get_key(v))) //v的key已经存在
-						return pair<iterator, bool>(iterator(nd->next, vector_iterator(&buckets + index), this), false);
+					if (equals(get_key(nd->next->value), get_key(v))) //v的key已经存在
+						return pair<iterator, bool>(iterator(nd->next, vector_iterator(&(buckets[0]) + index), this), false);
 					nd = nd->next;
 				}
 				//v的key不存在
 				nd->next = get_node(v);
-				return pair<iterator, bool>(iterator(nd->next, vector_iterator(&buckets + index), this), true);
+				return pair<iterator, bool>(iterator(nd->next, vector_iterator(&(buckets[0]) + index), this), true);
 			}
+		}
+
+		//左值equal插入
+		//直接插到对应bucket的开始
+		iterator insert_equal(const value_type &v)
+		{
+			size_type index = bkt_num(v);
+			hashnode *nd = buckets[index];//找到v散列的位置
+			if (!nd) //nd为空
+			{
+				buckets[index] = get_node(std::move(v));
+				return iterator(buckets[index], vector_iterator(&(buckets[0]) + index), this);
+			}
+			else //nd非空
+			{
+				if (equals(get_key(nd->value), get_key(v))) //v的key已经存在,在其后位置插入
+				{
+					return iterator(__insert_after(nd, std::move(v)), vector_iterator(&(buckets[0]) + index), this);
+				}
+				while (nd->next) //nd->next非空
+				{
+					if (equals(get_key(nd->next->value), get_key(v))) //v的key已经存在
+						return iterator(__insert_after(nd, std::move(v)), vector_iterator(&(buckets[0]) + index), this);
+					nd = nd->next;
+				}
+				//v的key不存在,此时nd是最后一个节点
+				return iterator(__insert_after(nd, std::move(v)), vector_iterator(&(buckets[0]) + index), this);
+			}
+		}
+
+		//右值unique插入
+		//如果key已存在，返回pair(已存在key的iterator, false)
+		//否则，在key对应bucket的最后，返回pair(插入位置iterator, true).
+		pair<iterator, bool> insert_unique(value_type&& v)
+		{
+			size_type index = bkt_num(v);
+			hashnode *nd = buckets[index];//找到v散列的位置
+			if (!nd) //nd为空
+			{
+				buckets[index] = get_node(std::move(v));
+				return pair<iterator, bool>(iterator(buckets[index], vector_iterator(&(buckets[0]) + index), this), true);
+			}
+			else //nd非空
+			{
+				if (equals(get_key(nd->value), get_key(v))) //v的key已经存在
+					return pair<iterator, bool>(iterator(nd, vector_iterator(&(buckets[0]) + index), this), false);
+				while (nd->next) //nd->next非空
+				{
+					if (equals(get_key(nd->next->value), get_key(v))) //v的key已经存在
+						return pair<iterator, bool>(iterator(nd->next, vector_iterator(&(buckets[0]) + index), this), false);
+					nd = nd->next;
+				}
+				//v的key不存在
+				nd->next = get_node(std::move(v));
+				return pair<iterator, bool>(iterator(nd->next, vector_iterator(&(buckets[0]) + index), this), true);
+			}
+		}
+		//右值equal插入
+		//直接插到对应bucket的开始
+		iterator insert_equal(value_type &&v)
+		{
+			size_type index = bkt_num(v);
+			hashnode *nd = buckets[index];//找到v散列的位置
+			if (!nd) //nd为空
+			{
+				buckets[index] = get_node(v);
+				return iterator(buckets[index], vector_iterator(&(buckets[0]) + index), this);
+			}
+			else //nd非空
+			{
+				if (equals(get_key(nd->value), get_key(v))) //v的key已经存在,在其后位置插入
+				{
+					return iterator(__insert_after(nd, v), vector_iterator(&(buckets[0]) + index), this);
+				}
+				while (nd->next) //nd->next非空
+				{
+					if (equals(get_key(nd->next->value), get_key(v))) //v的key已经存在
+						return iterator(__insert_after(nd, v), vector_iterator(&(buckets[0]) + index), this);
+					nd = nd->next;
+				}
+				//v的key不存在,此时nd是最后一个节点
+				return iterator(__insert_after(nd, v), vector_iterator(&(buckets[0]) + index), this);
+			}
+		}
+
+		//带提示的左值unique插入
+		//unique插入的提示不能减少计算量，直接调用无提示版本
+		iterator insert_unique(const_iterator hint, const value_type &v)
+		{
+			return insert_unique(v).first;
+		}
+
+		//带提示的左值equal插入
+		//提示不能减少计算量，直接调用无提示版本
+		iterator insert_equal(const_iterator hint, const value_type &v)
+		{
+			return insert_equal(v);
+		}
+
+
+		//带提示的右值unique插入
+		//提示不能减少计算量，直接调用无提示版本
+		iterator insert_unique(const_iterator hint, value_type &&v)
+		{
+			return insert_unique(std::move(v)).first;
+		}
+
+		//带提示的右值equal插入
+		//提示不能减少计算量，直接调用无提示版本
+		iterator insert_equal(const_iterator hint, value_type &&v)
+		{
+			return insert_equal(std::move(v));
+		}
+
+		//范围unique插入
+		template< class InputIt >
+		void insert_unique(InputIt first, InputIt last)
+		{
+			for (; first != last; ++first)
+				insert_unique(*first);
+		}
+
+		//范围equal插入
+		template<typename InputIt>
+		void insert_equal(InputIt first, InputIt last)
+		{
+			for (; first != last; ++first)
+				insert_equal(*first);
+		}
+
+		//Removes the element at pos.
+		//Returns iterator following the last removed element.
+		iterator erase(const_iterator pos)
+		{
+			iterator tmp = pos;
+			++tmp;
+			hashnode *nd = *(pos.block);//pos所在的桶
+			if (nd != pos.node) // pos.node不在起始位置
+			{
+				while (nd->next != pos.node)//找到pos的上一个节点
+					nd = nd->next;
+				nd->next = pos.node->next;//指向下一个节点
+			}
+			else
+			{
+				nd = pos.node->next;
+			}
+			delete_node(pos.node);
+			return tmp;
+		}
+
+
+		// Removes the elements in the range [first; last), which must be a valid range in *this.
+		iterator erase(const_iterator first, const_iterator last)
+		{
+			if (first != last)
+			{
+				vector_iterator block = first.block; //指向要删除的第一个桶
+				if (first.block == last.block) //first和last在一个桶内
+				{
+					__erase_range(first.node, last.node, block);
+				}
+				else	//first和last不在一个桶内
+				{
+					__erase_range(first.node, nullptr, block);//删除第1个桶从first开始的所有节点
+					while (++block != last.block) // 不到最后1个要删除的桶
+						if (*block) //block所指的桶非空
+							__erase_range(*block, nullptr, block); //删除这个桶的所有元素
+					__erase_range(*block, last.node, block);
+				}
+			}
+			return (iterator)last;
+		}
+
+		//Removes the element (if one exists) with the key equivalent to key.
+		size_type erase(const key_type& key)
+		{
+			;
+		}
+
+		//Returns a range containing all elements with key key in the container. The range is defined by two iterators, 
+		//the first pointing to the first element of the wanted range and the second pointing past the last element of the range. 
+		pair<iterator, iterator> equal_range(const key_type& v)
+		{
 
 		}
-		template< class InputIt >
-		void insert(InputIt first, InputIt last);
-		
+
+		iterator find(const key_type& k)
+		{
+			size_type index = bkt_num_key(k);
+			hashnode *nd = buckets[index];//找到v散列的桶
+			while (nd)
+			{
+				if (equals(get_key(nd->value), k)) //找到k
+					return iterator(nd, addressof(buckets[0]) + index, this);
+			}
+			//没找到
+			return end();
+		}
+
 		// Returns current maximum load factor. 
 		float max_load_factor() const { return load_factor; }
 		// Sets the maximum load factor to ml. 
@@ -233,7 +452,7 @@ namespace my_stl
 		}
 
 	private:
-		hashnode* get_node(const value_type v)
+		hashnode* get_node(const value_type &v)
 		{
 			//申请内存
 			hashnode *nd = alloc_node.allocate(1);
@@ -242,6 +461,33 @@ namespace my_stl
 			nd->next = nullptr;
 			return nd;
 		}
+
+		hashnode* get_node(value_type &&v)
+		{
+			typedef typename __type_traits<value_type>::is_POD_type is_POD;
+			return __get_node(std::move(v), is_POD());
+		}
+
+		hashnode *__get_node(value_type &&v, __true_type)
+		{
+			//申请内存
+			hashnode *nd = alloc_node.allocate(1);
+			//直接移动内存
+			memmove(addressof(nd->value), addressof(v), sizeof(value_type) / sizeof(unsigned char));
+			nd->next = nullptr;
+			return nd;
+		}
+
+		hashnode *__get_node(value_type &&v, __false_type)
+		{
+			//申请内存
+			hashnode *nd = alloc_node.allocate(1);
+			//构造节点值
+			alloc.construct(&(nd->value), v);
+			nd->next = nullptr;
+			return nd;
+		}
+
 		void delete_node(hashnode *nd)
 		{
 			//析构节点值
@@ -295,6 +541,52 @@ namespace my_stl
 			return bkt_num_key(get_key(v), n);
 		}
 
+		//在nd后面插入节点,值为v
+		hashnode *__insert_after(hashnode *nd, const value_type &v)
+		{
+			hashnode *tmp = get_node(v);
+			tmp->next = nd->next;
+			nd->next = tmp;
+			return tmp;
+		}
+
+		//在nd后面插入节点,值为v
+		hashnode *__insert_after(hashnode *nd, value_type &&v)
+		{
+			hashnode *tmp = get_node(std::move(v));
+			tmp->next = nd->next;
+			nd->next = tmp;
+			return tmp;
+		}
+
+		//Removes the elements in the range[first.node, last), which must be a valid range in a bucket.
+		void __erase_range(hashnode *first, hashnode *last, vector_iterator &block)
+		{
+			if (first != last)
+			{
+				hashnode *nd = *block;//first所在的桶
+				if (nd != first)     // first不在起始位置
+				{
+					while (nd->next != first)//找到first的上一个节点
+						nd = nd->next;
+					nd->next = last; //指向last
+				}
+				else
+					*block = last;
+				//销毁节点
+				for (; first != last; first = first->next)
+					delete_node(first);
+			}
+		}
+
+		//返回nd所在桶的最后一个非空节点（仅当桶非空时使用）
+		hashnode *__end_of_bucket(hashnode *nd)
+		{
+			if (nd)
+				while (nd->next)
+					nd = nd->next;
+			return nd;
+		}
 	};
 
 
